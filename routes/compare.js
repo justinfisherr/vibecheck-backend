7c0d9c295d1539c7437cc723513bf748886d768a
justinfisherrr <74376317+justinfisherrr@users.noreply.github.com>
2023-04-06 14:14:56 -0700

    Merge branch 'dev' of https://github.com/justinfisherrr/vibecheck-backend into dev

--- routes/compare.js
+++ routes/compare.js
@@ -1,29 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const matchMaker = require('../helpers/matchMaker');
-const User = require('../models/user');
+const getMatched = require("../middlware/getMatched");
 
-router.post('/compare', async (req, res) => {
-	try {
-		const userID = req.body.my_username;
-		const user2_ID = req.body.other_username;
-
-		const requestedUser = await User.findOne({
-			'user_info.vibe_id': user2_ID,
-		});
-
-		if (!requestedUser) throw new Error("User doesn't exist");
-
-		//We are safe here as we just registered the Original User
-		const originalUser = await User.findOne({
-			'user_info.vibe_id': userID,
-		});
-
-		const matchedObject = matchMaker(originalUser, requestedUser);
-		res.status(200).send({ data: matchedObject, success: true });
-	} catch (error) {
-		res.status(400).send({ message: error, success: false });
-	}
+router.post("/compare", getMatched, (req, res) => {
+  res.status(200).send({ data: req.matchedObject, success: true });
 });
 
 module.exports = router;
--- routes/getUsers.js
+++ routes/getUsers.js
@@ -1,34 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
-
+const findUsers = require("../middlware/findUsers");
 //This route assumes we will always be passed a query
-router.get("/getuser/:id", async (req, res) => {
-  let users = {};
-  const parse = {
-    user_info: 1,
-    _id: 0,
-  };
-  const conditionals = {
-    $or: [
-      {
-        "user_info.vibe_id": { $regex: `^${req.params.id}`, $options: "i" },
-      },
-      {
-        "user_info.username": {
-          $regex: `^${req.params.id}`,
-          $options: "i",
-        },
-      },
-    ],
-  };
-  try {
-    users = await User.find(conditionals, parse);
-    res.status(200).send({ data: users, success: true });
-  } catch (error) {
-    //We can only reach here on a server error
-    res.status(500).send({ message: error, success: false });
-  }
+router.get("/getuser/:id", findUsers, (req, res) => {
+  res.status(200).send({ data: req.users, success: true });
 });
 
 module.exports = router;
